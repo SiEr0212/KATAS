acefe7a9119a03282f78af564e61a14c34a314ef
SiEr0212 <57448248+SiEr0212@users.noreply.github.com>
2020-08-13 23:53:07 +0200

    Update Count characters-in-your-string.js

--- 6kyu/Count characters-in-your-string.js
+++ 6kyu/Count characters-in-your-string.js
@@ -3,3 +3,13 @@ The main idea is to count all the occurring characters in a string. If you have 
 What if the string is empty? Then the result should be empty object literal, {}.
 */
 
+//My Solution:
+const count = (string) => {  
+  let obj = {};
+  for(let e of string)if(!obj[e])obj[e] = 1;
+  else obj[e] = obj[e] + 1;
+  return obj
+}
+
+count("aba") 
+// returns { a: 2, b: 1 }
