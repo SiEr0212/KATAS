6d5635e4ac81626fafb489587654d4e12b25b6f2
SiEr0212 <simonertel@gmx.de>
2020-08-25 22:15:25 +0200

    List Filtering4

--- 7kyu/ List Filtering.js
+++ 7kyu/ List Filtering.js
@@ -11,3 +11,9 @@ filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 */
+
+function filter_list(l) {
+  return l.filter(function (el) {
+    return typeof el !== "string";
+  });
+}
