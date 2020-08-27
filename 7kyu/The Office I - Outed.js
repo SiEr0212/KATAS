f89db829247cc90161ab414a50706a0e4ce40b80
SiEr0212 <simonertel@gmx.de>
2020-08-27 23:19:48 +0200

    The Office I - Outed6

--- 7kyu/The Office I - Outed.js
+++ 7kyu/The Office I - Outed.js
@@ -31,3 +31,5 @@ function outed(meet, boss) {
     return "Nice Work Champ!";
   }
 }
+
+outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura');
