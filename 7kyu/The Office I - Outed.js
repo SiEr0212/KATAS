4e2771cb828b17c37b7ae7af112c96563c0202c9
SiEr0212 <simonertel@gmx.de>
2020-08-27 23:20:16 +0200

    The Office I - Outed7

--- 7kyu/The Office I - Outed.js
+++ 7kyu/The Office I - Outed.js
@@ -33,3 +33,4 @@ function outed(meet, boss) {
 }
 
 outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura');
+// returns 'Get Out Now!'
