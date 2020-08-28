5cde941d047fbc21f9cba2595e67e15ed037c630
SiEr0212 <simonertel@gmx.de>
2020-08-28 19:49:52 +0200

    Simple Fun #144: Distinct Digit Year4

--- 7kyu/Distinct Digit Year.js
+++ 7kyu/Distinct Digit Year.js
@@ -1,6 +1,8 @@
 /*
 Simple Fun #144: Distinct Digit Year
 
+from codewars: https://www.codewars.com/kata/58aa68605aab54a26c0001a6/train/javascript
+
 The year of 2013 is the first year after the old 1987 with only distinct digits.
 
 Now your task is to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
