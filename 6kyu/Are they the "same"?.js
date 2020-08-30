6cf6c75b0a21fd2e39ff3f4f3a0310e44ac29b7a
SiEr0212 <simonertel@gmx.de>
2020-08-30 23:12:51 +0200

    Are they the same?5

--- 6kyu/Are they the "same"?.js
+++ 6kyu/Are they the "same"?.js
@@ -1,4 +1,12 @@
 /*
 Are they the "same"?
 
+from codewars: https://www.codewars.com/kata/550498447451fbbd7600041c/train/javascript
+
+Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 */
