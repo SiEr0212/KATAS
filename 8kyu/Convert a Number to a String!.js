4934e263835e2d7ec3f9b51fbc271a2af175b61b
SiEr0212 <simonertel@gmx.de>
2020-09-11 12:32:40 +0200

    Convert a Number to a String 3

--- 8kyu/Convert a Number to a String!.js
+++ 8kyu/Convert a Number to a String!.js
@@ -1,3 +1,13 @@
 /*
 Convert a Number to a String!
+
+from codewars: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+numberToString(123); // returns '123';`   
+numberToString(999); // returns '999';`
 */
