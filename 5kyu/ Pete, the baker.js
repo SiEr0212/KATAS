f6b0e2d8f8f7da3b3ea51508ae6bfe9aebba1f72
SiEr0212 <simonertel@gmx.de>
2020-09-12 18:33:01 +0200

    Pete, the baker 8

--- 5kyu/ Pete, the baker.js
+++ 5kyu/ Pete, the baker.js
@@ -25,6 +25,7 @@ const cakes = (recipe, ingredients) => {
   }
   return Math.min(...amounts);
 };
+
 //recipe = {flour: 500, sugar: 200, eggs: 1};
 //available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
 cakes(recipe, available);
