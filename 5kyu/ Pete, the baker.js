ff899773104fcc88053f05ef95b6f0c6b91b68bb
SiEr0212 <simonertel@gmx.de>
2020-09-12 18:32:27 +0200

    Pete, the baker 6

--- 5kyu/ Pete, the baker.js
+++ 5kyu/ Pete, the baker.js
@@ -16,3 +16,16 @@ cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flo
 */
 
 //My solution: 
+
+const cakes = (recipe, ingredients) => {
+    let amounts = []
+    for (let item in recipe) {
+        if (!ingredients[item]) return 0;
+        amounts.push(Math.floor(ingredients[item] / recipe[item]));
+    }
+    return Math.min(...amounts)
+}
+//recipe = {flour: 500, sugar: 200, eggs: 1};
+//available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
+cakes(recipe, available);
+// returns 2
