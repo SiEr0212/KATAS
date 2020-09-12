cfc177111b49e33700c2ff2b846dc1ab1a500e20
SiEr0212 <simonertel@gmx.de>
2020-09-12 18:32:46 +0200

    Pete, the baker 7

--- 5kyu/ Pete, the baker.js
+++ 5kyu/ Pete, the baker.js
@@ -15,16 +15,16 @@ cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, mil
 cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
 */
 
-//My solution: 
+//My solution:
 
 const cakes = (recipe, ingredients) => {
-    let amounts = []
-    for (let item in recipe) {
-        if (!ingredients[item]) return 0;
-        amounts.push(Math.floor(ingredients[item] / recipe[item]));
-    }
-    return Math.min(...amounts)
-}
+  let amounts = [];
+  for (let item in recipe) {
+    if (!ingredients[item]) return 0;
+    amounts.push(Math.floor(ingredients[item] / recipe[item]));
+  }
+  return Math.min(...amounts);
+};
 //recipe = {flour: 500, sugar: 200, eggs: 1};
 //available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
 cakes(recipe, available);
