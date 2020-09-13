e60416f4b47d582e53204d9f2600b8cb146b3b6c
SiEr0212 <simonertel@gmx.de>
2020-09-13 23:02:04 +0200

    Square(n) Sum 8

--- 8kyu/Square(n) Sum.js
+++ 8kyu/Square(n) Sum.js
@@ -11,5 +11,8 @@ For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 //My solution:
 
-const squareSum =(numbers) =>{return numbers.map(x=> x ** 2).reduce((a, b) => a + b, 0)}
+const squareSum = (numbers) => {
+  return numbers.map((x) => x ** 2).reduce((a, b) => a + b, 0);
+};
 
+squareSum([0, 3, 4, 5]);
