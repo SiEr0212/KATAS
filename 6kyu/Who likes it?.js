90fa63afbfc56cadea6d93d9b8b736bf1f408380
SiEr0212 <simonertel@gmx.de>
2020-09-18 19:23:05 +0200

    Who likes it? 1

---
+++ 6kyu/Who likes it?.js
@@ -0,0 +1,20 @@
+/*
+
+*/
+const likes = (names) => {
+  const oneName = names.slice(0) + " likes this";
+  const twoTo3Names =
+    names.slice(0, -1).join(", ") + " and " + names.slice(-1) + " like this";
+  const moreThan3Names =
+    names.slice(0, 2).join(", ") +
+    " and " +
+    (names.length - 2) +
+    " other like this";
+  return names.length == 1
+    ? oneName
+    : names.length <= 3 && names.length > 1
+    ? twoTo3Names
+    : names.length > 3
+    ? moreThan3Names
+    : "no one likes this";
+};
