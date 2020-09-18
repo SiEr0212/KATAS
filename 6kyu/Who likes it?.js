d4de46ad46101c04ba792a28601168e210712970
SiEr0212 <simonertel@gmx.de>
2020-09-18 19:25:00 +0200

    Who likes it? 5

--- 6kyu/Who likes it?.js
+++ 6kyu/Who likes it?.js
@@ -14,20 +14,4 @@ likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
 likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
 For 4 or more names, the number in and 2 others simply increases.
 */
-const likes = (names) => {
-  const oneName = names.slice(0) + " likes this";
-  const twoTo3Names =
-    names.slice(0, -1).join(", ") + " and " + names.slice(-1) + " like this";
-  const moreThan3Names =
-    names.slice(0, 2).join(", ") +
-    " and " +
-    (names.length - 2) +
-    " other like this";
-  return names.length == 1
-    ? oneName
-    : names.length <= 3 && names.length > 1
-    ? twoTo3Names
-    : names.length > 3
-    ? moreThan3Names
-    : "no one likes this";
-};
+
