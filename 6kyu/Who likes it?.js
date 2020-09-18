dea2be012cb42522c27517312ea17ed62fc376ac
SiEr0212 <simonertel@gmx.de>
2020-09-18 19:27:37 +0200

    Who likes it? 10

--- 6kyu/Who likes it?.js
+++ 6kyu/Who likes it?.js
@@ -17,22 +17,22 @@ For 4 or more names, the number in and 2 others simply increases.
 //My solution:
 
 const likes = (names) => {
-    const oneName = names.slice(0) + " likes this";
-    const twoTo3Names =
-      names.slice(0, -1).join(", ") + " and " + names.slice(-1) + " like this";
-    const moreThan3Names =
-      names.slice(0, 2).join(", ") +
-      " and " +
-      (names.length - 2) +
-      " other like this";
-    return names.length == 1
-      ? oneName
-      : names.length <= 3 && names.length > 1
-      ? twoTo3Names
-      : names.length > 3
-      ? moreThan3Names
-      : "no one likes this";
-  };
+  const oneName = names.slice(0) + " likes this";
+  const twoTo3Names =
+    names.slice(0, -1).join(", ") + " and " + names.slice(-1) + " like this";
+  const moreThan3Names =
+    names.slice(0, 2).join(", ") +
+    " and " +
+    (names.length - 2) +
+    " other like this";
+  return names.length == 1
+    ? oneName
+    : names.length <= 3 && names.length > 1
+    ? twoTo3Names
+    : names.length > 3
+    ? moreThan3Names
+    : "no one likes this";
+};
 
-  likes (["Alex", "Jacob", "Mark", "Max"]);
-  //returns "Alex, Jacob and 2 others like this"
+likes(["Alex", "Jacob", "Mark", "Max"]);
+//returns "Alex, Jacob and 2 others like this"
