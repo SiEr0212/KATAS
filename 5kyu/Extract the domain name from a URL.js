8ad2738590dca855c40aa33ebb1879a9553f0808
SiEr0212 <simonertel@gmx.de>
2020-09-20 15:54:07 +0200

    Extract the domain name from a URL 3

--- 5kyu/Extract the domain name from a URL.js
+++ 5kyu/Extract the domain name from a URL.js
@@ -1,19 +1,7 @@
 
 /*
+Extract the domain name from a URL
 
 */
 
 
-const domainName = (url) => {
-  if (url.indexOf("http") == -1 && url.indexOf("www") == -1) {
-    return url.split(".")[0];
-  } else if (
-    (url.indexOf(".") == 11 && url.indexOf("3") != 10) ||
-    url.indexOf("www") == 0 ||
-    url.indexOf(".") == 10
-  ) {
-    return url.split(".")[1].split(".")[0];
-  } else {
-    return url.split(".")[0].split("//")[1];
-  }
-};
