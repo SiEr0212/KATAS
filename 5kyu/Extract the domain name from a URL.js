17de90a655fd7c72c4bdb9360a0e7027bf98438c
SiEr0212 <simonertel@gmx.de>
2020-09-20 16:02:31 +0200

    Extract the domain name from a URL 10

--- 5kyu/Extract the domain name from a URL.js
+++ 5kyu/Extract the domain name from a URL.js
@@ -1,4 +1,3 @@
-
 /*
 Extract the domain name from a URL
 
@@ -13,29 +12,33 @@ domainName("https://www.cnet.com") == "cnet"
 //My solution:
 
 const domainName = (url) => {
-    if (url.indexOf("http") == -1 && url.indexOf("www") == -1) {
-       return url.split(".")[0];
-    }else if(url.indexOf(".")==11  && url.indexOf('3')!=10 || url.indexOf("www") == 0 ||url.indexOf(".")==10  )
-     { return url.split(".")[1].split(".")[0];
-      } else {
-       return url.split(".")[0].split("//")[1];
-     }
-   };
+  if (url.indexOf("http") == -1 && url.indexOf("www") == -1) {
+    return url.split(".")[0];
+  } else if (
+    (url.indexOf(".") == 11 && url.indexOf("3") != 10) ||
+    url.indexOf("www") == 0 ||
+    url.indexOf(".") == 10
+  ) {
+    return url.split(".")[1].split(".")[0];
+  } else {
+    return url.split(".")[0].split("//")[1];
+  }
+};
 
-   domainName('http://www.uthdbrx93iddfy2lg-w4-ega6f.co');
-   //returns 'uthdbrx93iddfy2lg-w4-ega6f' 
-   // domainName('https://123.net') returns '123'
-   // domainName('xhpp09i26cm.edu') returns 'xhpp09i26cm'
-   //domainName("www.xakep.ru") returns 'xakep'
-   //domainName("http://google.com") returns 'google'
-   //domainName('https://www.60h6d96xinocar1k.co.uk/users') returns '60h6d96xinocar1k.'
-   //domainName('ishok6dgyd8v49gn5-3.co.uk') returns 'ishok6dgyd8v49gn5-3.'
+domainName("http://www.uthdbrx93iddfy2lg-w4-ega6f.co");
+//returns 'uthdbrx93iddfy2lg-w4-ega6f'
+// domainName('https://123.net') returns '123'
+// domainName('xhpp09i26cm.edu') returns 'xhpp09i26cm'
+//domainName("www.xakep.ru") returns 'xakep'
+//domainName("http://google.com") returns 'google'
+//domainName('https://www.60h6d96xinocar1k.co.uk/users') returns '60h6d96xinocar1k.'
+//domainName('ishok6dgyd8v49gn5-3.co.uk') returns 'ishok6dgyd8v49gn5-3.'
 
 // Better solution:
 
-const domainName = (url) =>{
-    url = url.replace("https://", '');
-    url = url.replace("http://", '');
-    url = url.replace("www.", '');
-    return url.split('.')[0];
-  };
+const domainName = (url) => {
+  url = url.replace("https://", "");
+  url = url.replace("http://", "");
+  url = url.replace("www.", "");
+  return url.split(".")[0];
+};
