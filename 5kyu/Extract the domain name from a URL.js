48be6b7d09e45814591b668dc2003eecb1bf417c
SiEr0212 <simonertel@gmx.de>
2020-09-20 15:56:56 +0200

    Extract the domain name from a URL 7

--- 5kyu/Extract the domain name from a URL.js
+++ 5kyu/Extract the domain name from a URL.js
@@ -12,6 +12,15 @@ domainName("https://www.cnet.com") == "cnet"
 */
 //My solution:
 
+const domainName = (url) => {
+    if (url.indexOf("http") == -1 && url.indexOf("www") == -1) {
+       return url.split(".")[0];
+    }else if(url.indexOf(".")==11  && url.indexOf('3')!=10 || url.indexOf("www") == 0 ||url.indexOf(".")==10  )
+     { return url.split(".")[1].split(".")[0];
+      } else {
+       return url.split(".")[0].split("//")[1];
+     }
+   };
 
 
 
