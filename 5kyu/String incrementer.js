a47f03bff831b4ffaa2e97b63948438f4aa758c1
SiEr0212 <simonertel@gmx.de>
2020-09-21 21:31:05 +0200

    String incrementer 7

--- 5kyu/String incrementer.js
+++ 5kyu/String incrementer.js
@@ -22,3 +22,11 @@ foo099 -> foo100
 Attention: If the number has leading zeros the amount of digits should be considered.
 */
 //My solution:
+
+function incrementString(input) {
+    if (isNaN(parseInt(input[input.length - 1]))) return input + "1";
+    return input.replace(/(0*)([0-9]+$)/, function (match, p1, p2) {
+      var up = parseInt(p2) + 1;
+      return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+    });
+  }
