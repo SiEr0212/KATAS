52a1843d686b45ef2372a1d3a8c65e0f547a7a17
SiEr0212 <simonertel@gmx.de>
2020-09-23 16:39:48 +0200

    Sort the Odd 7

--- 6kyu/Sort thr Odd.js
+++ 6kyu/Sort thr Odd.js
@@ -13,3 +13,9 @@ Example
 sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
 */
 //My Solution:
+
+const sortArray = (array) => {
+    const odds = array.filter((x) => x % 2).sort((a, b) => a - b);
+    return array.map((x) => (x % 2 ? odds.shift() : x));
+  };
+  
