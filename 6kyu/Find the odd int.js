c50f462952bc709ed6fea30725f66b0ac1149c66
SiEr0212 <simonertel@gmx.de>
2020-09-30 21:44:24 +0200

    Find the odd Int 11

--- 6kyu/Find the odd int.js
+++ 6kyu/Find the odd int.js
@@ -10,15 +10,15 @@ There will always be only one integer that appears an odd number of times.
 //My solution:
 
 const findOdd = (A) => {
-    let report = {};
-    A.forEach(function (el) {
-      report[el] = report[el] + 1 || 1;
-    });
-  
-    for (let i in report) {
-      if (report[i] % 2 != 0) return Number(i);
-    }
-  };
+  let report = {};
+  A.forEach(function (el) {
+    report[el] = report[el] + 1 || 1;
+  });
 
-findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]);
+  for (let i in report) {
+    if (report[i] % 2 != 0) return Number(i);
+  }
+};
+
+findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]);
 //returns: 5
