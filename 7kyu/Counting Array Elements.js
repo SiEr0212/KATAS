c523a7fb80a05a7f209893b8a564acbf6be4b05c
SiEr0212 <simonertel@gmx.de>
2020-10-01 23:52:42 +0200

    Counting Array Elements 5

--- 7kyu/Counting Array Elements.js
+++ 7kyu/Counting Array Elements.js
@@ -2,4 +2,9 @@
 Counting Array Elements
 
 from codewars: https://www.codewars.com/kata/5569b10074fe4a6715000054/train/javascript
+
+Write a function that takes an array and counts the number of each unique element present.
+
+count(['james', 'james', 'john']) 
+#=> { 'james': 2, 'john': 1}
 */
