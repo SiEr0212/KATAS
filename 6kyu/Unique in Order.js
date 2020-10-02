21b83baeaeca58f75f3dea8e975176c697c52a67
SiEr0212 <simonertel@gmx.de>
2020-10-02 23:20:52 +0200

    Unique in Order 5

--- 6kyu/Unique in Order.js
+++ 6kyu/Unique in Order.js
@@ -11,3 +11,4 @@ uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 */
+//My solution:
