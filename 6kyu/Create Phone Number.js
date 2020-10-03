0dc9d125a6d94bf5960d26fd29dece86dd7bbc28
SiEr0212 <simonertel@gmx.de>
2020-10-03 19:01:11 +0200

    Create Phone Number 8

--- 6kyu/Create Phone Number.js
+++ 6kyu/Create Phone Number.js
@@ -12,3 +12,14 @@ Don't forget the space after the closing parentheses!
 */
 //My solution:
 
+const createPhoneNumber = (numbers) => {
+    numbers = numbers.join("");
+    return (
+      "(" +
+      numbers.substring(0, 3) +
+      ") " +
+      numbers.substring(3, 6) +
+      "-" +
+      numbers.substring(6)
+    );
+  };
