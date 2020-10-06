fa505fee34328f2d67bdb6150d6df8e49b784076
SiEr0212 <simonertel@gmx.de>
2020-10-06 23:05:16 +0200

    Is n divisible by x and y? 10

--- 8kyu/Is n divisible by x and y?.js
+++ 8kyu/Is n divisible by x and y?.js
@@ -14,10 +14,8 @@ Examples:
 //My solution:
 
 const isDivisible = (n, x, y) => {
-    return n % x == 0 && n % y == 0 ? true : false;
-  };
+  return n % x == 0 && n % y == 0 ? true : false;
+};
 
-isDivisible(3,3,4);
+isDivisible(3, 3, 4);
 //returns: false
-
-
