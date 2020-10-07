70266be2580eb399bb4119d8c130e2cefb03d585
SiEr0212 <simonertel@gmx.de>
2020-10-07 22:56:46 +0200

    count '9's from 1 to n 10

--- 5kyu/count '9's from 1 to n.js
+++ 5kyu/count '9's from 1 to n.js
@@ -20,20 +20,20 @@ number9(100) //should return 20
 //My solution:
 
 const number9Helper = (num) => {
-    let pow = Math.floor(Math.log10(num));
-    let round = Math.pow(10, pow);
-    let times = Math.floor(num / round);
-    let rest = Math.abs(num - round * times);
-    let res = pow * (round == 10 ? 1 : round / 10) * times;
-    if (num.toString()[0] == "9") res += rest;
-    if (rest < 9) return res;
-    else return res + number9Helper(rest);
-  };
-  const number9 = (num) => {
-    let res = number9Helper(num);
-    res = res + (num.toString().split("9").length - 1);
-    return res;
-  };
+  let pow = Math.floor(Math.log10(num));
+  let round = Math.pow(10, pow);
+  let times = Math.floor(num / round);
+  let rest = Math.abs(num - round * times);
+  let res = pow * (round == 10 ? 1 : round / 10) * times;
+  if (num.toString()[0] == "9") res += rest;
+  if (rest < 9) return res;
+  else return res + number9Helper(rest);
+};
+const number9 = (num) => {
+  let res = number9Helper(num);
+  res = res + (num.toString().split("9").length - 1);
+  return res;
+};
 
 number9(565754);
 //returns: 275645
