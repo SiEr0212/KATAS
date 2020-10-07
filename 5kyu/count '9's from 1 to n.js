e66a30a381f0632ec97e48f8b3024dd53d43fd28
SiEr0212 <simonertel@gmx.de>
2020-10-07 22:53:58 +0200

    count '9's from 1 to n 5

--- 5kyu/count '9's from 1 to n.js
+++ 5kyu/count '9's from 1 to n.js
@@ -2,4 +2,18 @@
 count '9's from 1 to n
 
 from codwars: https://www.codewars.com/kata/55143152820d22cdf00001bb/train/javascript
+
+It's 9 time!
+
+I want to count from 1 to n. How many times will I use a '9'?
+
+9, 19, 91.. will contribute one '9' each, 99, 199, 919.. wil contribute two '9's each...etc
+
+Note: n will always be a postive integer.
+
+number9(8) //should return 0
+number9(9) //should return 1
+number9(10) //should return 1
+number9(98) //should return 18
+number9(100) //should return 20
 */
