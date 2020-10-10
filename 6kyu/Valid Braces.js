dade6fe685e16c64ad08c3fe88cfdfdc3af36851
SiEr0212 <simonertel@gmx.de>
2020-10-10 23:32:28 +0200

    Valid Braces 10

--- 6kyu/Valid Braces.js
+++ 6kyu/Valid Braces.js
@@ -22,17 +22,15 @@ Examples
 //My solution:
 
 const validBraces = (braces) => {
-    while (
-      braces.indexOf("{}") != -1 ||
-      braces.indexOf("()") != -1 ||
-      braces.indexOf("[]") != -1
-    ) {
-      braces = braces.replace("{}", "").replace("()", "").replace("[]", "");
-    }
-    return braces.length == 0;
-  };
+  while (
+    braces.indexOf("{}") != -1 ||
+    braces.indexOf("()") != -1 ||
+    braces.indexOf("[]") != -1
+  ) {
+    braces = braces.replace("{}", "").replace("()", "").replace("[]", "");
+  }
+  return braces.length == 0;
+};
 
 validBraces("[({})](]");
 //returns: false
-
-  
