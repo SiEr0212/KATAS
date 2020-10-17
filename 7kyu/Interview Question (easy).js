04913592e82dcc6093a107813ac6c14ed503464e
SiEr0212 <simonertel@gmx.de>
2020-10-17 21:53:33 +0200

    Sentences should start with capital letters 1

--- 7kyu/Interview Question (easy).js
+++ 7kyu/Interview Question (easy).js
@@ -1,22 +1,21 @@
-/*
-Interview Question (easy)
+function getStrings(city){
+ let newS=[]
+ newS.push(city.toLowerCase().split("").filter(function(x, n, self) {
+   console.log(n)
+   return self.indexOf(x) == n 
+  }).join(""))
+  
+ for(let char of city){
+   
+   //console.log(newS)
+ }
+ //let newS1 = newS.join('').toLowerCase()
+ return newS.join(' :*,')+':*'
+}
 
-from codewars: https://www.codewars.com/kata/5b358a1e228d316283001892/train/javascript
-
-ou receive the name of a city as a string, and you need to return a string that shows how many times each letter shows up in the string by using an asterisk (*).
-
-For example:
-
-"Chicago"  -->  "c:**,h:*,i:*,a:*,g:*,o:*"
-As you can see, the letter c is shown only once, but with 2 asterisks.
-
-The return string should include only the letters (not the dashes, spaces, apostrophes, etc). There should be no spaces in the output, and the different letters are separated by a comma (,) as seen in the example above.
-
-Note that the return string must list the letters in order of their first appearence in the original string.
-
-More examples:
-
-"Bangkok"    -->  "b:*,a:*,n:*,g:*,k:**,o:*"
-"Las Vegas"  -->  "l:*,a:**,s:**,v:*,e:*,g:*"
-*/
-//My solution:
+getStrings("Chicago");
+//"c:**,h:*,i:*,a:*,g:*,o:*");
+getStrings("Bangkok");
+//"b:*,a:*,n:*,g:*,k:**,o:*");
+ //getStrings("Las Vegas"), //"l:*,a:**,s:**,v:*,e:*,g:*");
+ 
