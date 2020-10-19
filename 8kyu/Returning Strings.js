92eaf81ab1f7710d99ee4e1ece846aa4b84e859e
SiEr0212 <simonertel@gmx.de>
2020-10-19 23:28:12 +0200

    Returning Strings 3

--- 8kyu/Returning Strings.js
+++ 8kyu/Returning Strings.js
@@ -1,2 +1,13 @@
 /*
+Returning Strings
+
+from codewars: https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+SQL: return results in a column named greeting
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
 */
+
+
