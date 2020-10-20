558eaac81803a2cefe86b7e169abd76d8a10effb
SiEr0212 <simonertel@gmx.de>
2020-10-20 22:24:56 +0200

    Shortest Word 10

--- 7kyu/Shortest Word.js
+++ 7kyu/Shortest Word.js
@@ -1,7 +1,6 @@
 /*
 Shortest Word
 
-
 from codewars: https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/javascript
 
 Simple, given a string of words, return the length of the shortest word(s).
