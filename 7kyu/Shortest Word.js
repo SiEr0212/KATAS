eb63cfaa6161170af5ebc51bb5153d58e8ee54d4
SiEr0212 <simonertel@gmx.de>
2020-10-20 22:22:32 +0200

    Shortest Word 5

--- 7kyu/Shortest Word.js
+++ 7kyu/Shortest Word.js
@@ -7,3 +7,12 @@ Simple, given a string of words, return the length of the shortest word(s).
 
 String will never be empty and you do not need to account for different data types.
 */
+
+const findShort = (s) => {
+    let lengths = [];
+    for (let word of s.split(" ")) {
+      lengths.push(word.length);
+    }
+    return Math.min(...lengths);
+  };
+  
