a08a0cb8586d4f7745da4347acef4a31f2c73bb7
SiEr0212 <simonertel@gmx.de>
2020-10-23 14:54:15 +0200

    Simple Pig Latin 7

--- 5kyu/Simple Pig Latin.js
+++ 5kyu/Simple Pig Latin.js
@@ -10,3 +10,11 @@ pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 pigIt('Hello world !');     // elloHay orldway !
 */
 //My solution:
+
+const pigIt = (str) => {
+    return str
+      .split(" ")
+      .map((x) => (/[a-zA-Z]+/.test(x) ? x.slice(1) + x[0] + "ay" : x))
+      .join(" ");
+  };
+
