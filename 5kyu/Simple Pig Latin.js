edfdb6e1d1b74f5b2422bd73bc05aaa3d9802529
SiEr0212 <simonertel@gmx.de>
2020-10-23 14:53:12 +0200

    Simple Pig Latin 5

--- 5kyu/Simple Pig Latin.js
+++ 5kyu/Simple Pig Latin.js
@@ -3,5 +3,9 @@ Simple Pig Latin
 
 from codewars:https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
 
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
 */
