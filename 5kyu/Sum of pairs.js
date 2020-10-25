25d1eb85c82eafbe66c9a502b74adbba5cd9e198
SiEr0212 <simonertel@gmx.de>
2020-10-25 19:52:29 +0100

    Sum of pairs 7

--- 5kyu/Sum of pairs.js
+++ 5kyu/Sum of pairs.js
@@ -29,3 +29,4 @@ Negative numbers and duplicate numbers can and will appear.
 
 NOTE: There will also be lists tested of lengths upwards of 10,000,000 elements. Be sure your code doesn't time out.
 */
+//My solution:
