d9cbc85d09c36c0f332001f3caa0af0bfa4ed64b
SiEr0212 <simonertel@gmx.de>
2020-10-28 23:55:54 +0100

    Reverser 5

--- 7kyu/Reverser.js
+++ 7kyu/Reverser.js
@@ -5,3 +5,13 @@ from codewars: https://www.codewars.com/kata/58069e4cf3c13ef3a6000168/train/java
 
 Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. You should do this without converting the inputted number into a string.
 */
+
+const reverse = (n) => {
+    for (var r = 0; n; n = Math.floor(n / 10)) {
+      r *= 10;
+      r += n % 10;
+    }
+    return r;
+  };
+
+reverse(1234); 
