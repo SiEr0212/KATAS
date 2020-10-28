614128ff912eff8053c525e5403b7dd45675f536
SiEr0212 <simonertel@gmx.de>
2020-10-28 23:54:45 +0100

    Reverser 5

--- 7kyu/Reverser.js
+++ 7kyu/Reverser.js
@@ -3,5 +3,5 @@ Reverser
 
 from codewars: https://www.codewars.com/kata/58069e4cf3c13ef3a6000168/train/javascript
 
-
+Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. You should do this without converting the inputted number into a string.
 */
