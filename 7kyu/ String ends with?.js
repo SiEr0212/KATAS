5d7959aca0e122cc30eb67bb797e87fd65bef4e9
SiEr0212 <simonertel@gmx.de>
2020-10-30 23:47:29 +0100

    String ends with? 5

--- 7kyu/ String ends with?.js
+++ 7kyu/ String ends with?.js
@@ -1,6 +1,7 @@
 /*
 String ends with?
 
+from codewars: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
 
 Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
