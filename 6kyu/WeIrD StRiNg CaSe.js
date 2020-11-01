0bf60b97b14d9357a851eaa8dfbde061c570e8bf
SiEr0212 <simonertel@gmx.de>
2020-11-01 18:39:24 +0100

    WeIrD StRiNg CaSe 10

--- 6kyu/WeIrD StRiNg CaSe.js
+++ 6kyu/WeIrD StRiNg CaSe.js
@@ -13,17 +13,17 @@ toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"
 */
 //My solution:
 
-const toWeirdCase = (string) =>{
-    return string
-      .split(" ")
-      .map((v, i) =>
-        v
-          .split("")
-          .map((v, i) => (i % 2 === 0 ? v.toUpperCase() : v.toLowerCase()))
-          .join("")
-      )
-      .join(" ");
-  };
+const toWeirdCase = (string) => {
+  return string
+    .split(" ")
+    .map((v, i) =>
+      v
+        .split("")
+        .map((v, i) => (i % 2 === 0 ? v.toUpperCase() : v.toLowerCase()))
+        .join("")
+    )
+    .join(" ");
+};
 
-toWeirdCase('This is a test'); 
+toWeirdCase("This is a test");
 //returns: 'ThIs Is A TeSt'
