6d64791c7d415e01d7e6d182389a83c630a916af
SiEr0212 <simonertel@gmx.de>
2020-11-02 20:10:26 +0100

    Moving Zeros To The End 6

--- 5kyu/Moving Zeros To The End.js
+++ 5kyu/Moving Zeros To The End.js
@@ -8,3 +8,12 @@ Write an algorithm that takes an array and moves all of the zeros to the end, pr
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 */
 //My solution:
+
+const moveZeros = (arr) => {
+    let zeros = [];
+    let rest = [];
+    for (let e of arr) {
+      e === 0 ? zeros.push(e) : rest.push(e);
+    }
+    return rest.concat(zeros);
+  };
