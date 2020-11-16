59719266255a450e2e82d9b86e542541c0d1bfc6
SiEr0212 <simonertel@gmx.de>
2020-11-16 21:31:51 +0100

    Break camelCase

--- 6kyu/Break camelCase.js
+++ 6kyu/Break camelCase.js
@@ -8,5 +8,15 @@ Complete the solution so that the function will break up camel casing, using a s
 Example
 solution("camelCasing")  ==  "camel Casing"
 */
+//My solution:
 
-
+const solution = (str) => {
+    let cleaned = [];
+    for (let i = 0; i < str.length; i++) {
+      str[i] === str[i].toUpperCase()
+        ? cleaned.push(" " + str[i])
+        : cleaned.push(str[i]);
+    }
+    return cleaned.join("");
+  };
+  
