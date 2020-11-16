7a5ec5e6b777c8e0ccdc3c31ee7b70a75f031646
SiEr0212 <simonertel@gmx.de>
2020-11-16 21:31:16 +0100

    Break camelCase

--- 6kyu/Break camelCase.js
+++ 6kyu/Break camelCase.js
@@ -3,5 +3,10 @@ Break camelCase
 
 from codewars: https://www.codewars.com/kata/5208f99aee097e6552000148/train/javascript
 
+Complete the solution so that the function will break up camel casing, using a space between words.
 
+Example
+solution("camelCasing")  ==  "camel Casing"
 */
+
+
