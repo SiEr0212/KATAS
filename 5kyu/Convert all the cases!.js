f23d4ef873736f02def2be9f2dbf90ec96e1632f
SiEr0212 <simonertel@gmx.de>
2020-11-18 23:59:36 +0100

    Convert all the cases!

--- 5kyu/Convert all the cases!.js
+++ 5kyu/Convert all the cases!.js
@@ -26,67 +26,66 @@ Your function must deal with invalid input as shown, though it will only be pass
 //Mys solution:
 
 const camelCase = (s) => {
-    let cleaned = [];
-    if (s.includes("-")) {
-      cleaned.push(s.split("-"));
-    } else if (s.includes("_")) {
-      cleaned.push(s.split("_"));
-    } else {
-      cleaned.push(s.split(" "));
-    }
-  
-    let flattened = [].concat.apply([], cleaned);
-    let res = [];
-    res.push(flattened[0]);
-    for (let word of flattened.slice(1)) {
-      res.push(word[0].toUpperCase() + word.substring(1));
-    }
-    return res.join("");
-  };
-  
-  const snakeCase = (s) => {
-    if(s.includes('_')) return s
-    let cleaned2 = [];
-    for (let i = 0; i < s.length; i++) {
-      //console.log(s[i])
-      s[i] === s[i].toUpperCase()
-        ? cleaned2.push("_" + s[i].toLowerCase())
-        : cleaned2.push(s[i].toLowerCase());
-    }
-    return cleaned2.join("").split("-").join("");
-  };
-  
-  const kebabCase = (s) => {
-    let cleaned3 = [];
-    for (let i = 0; i < s.length; i++) {
-      s[i] === s[i].toUpperCase()
-        ? cleaned3.push("-" + s[i].toLowerCase())
-        : cleaned3.push(s[i].toLowerCase());
-    }
-    return cleaned3.join("").split("_").join("");
-  };
-  
-  const changeCase = (identifier, targetCase) => {
-    if (identifier.includes("-") && identifier.includes("_")) {
-      return undefined;
-    }
-    if (
-      (identifier.includes("-") || identifier.includes("_")) &&
-      identifier !== identifier.toLowerCase()
-    ) {
-      return undefined;
-    }
-    if (
-      targetCase !== "camel" &&
-      targetCase !== "snake" &&
-      targetCase !== "kebab"
-    )
-      return undefined;
-    if (targetCase === "camel") return camelCase(identifier);
-    if (targetCase === "snake") return snakeCase(identifier);
-    if (targetCase === "kebab") return kebabCase(identifier);
-  };
+  let cleaned = [];
+  if (s.includes("-")) {
+    cleaned.push(s.split("-"));
+  } else if (s.includes("_")) {
+    cleaned.push(s.split("_"));
+  } else {
+    cleaned.push(s.split(" "));
+  }
 
-  changeCase("some-lisp-name", "camel"); 
-  //returns: "someLispName"
+  let flattened = [].concat.apply([], cleaned);
+  let res = [];
+  res.push(flattened[0]);
+  for (let word of flattened.slice(1)) {
+    res.push(word[0].toUpperCase() + word.substring(1));
+  }
+  return res.join("");
+};
 
+const snakeCase = (s) => {
+  if (s.includes("_")) return s;
+  let cleaned2 = [];
+  for (let i = 0; i < s.length; i++) {
+    //console.log(s[i])
+    s[i] === s[i].toUpperCase()
+      ? cleaned2.push("_" + s[i].toLowerCase())
+      : cleaned2.push(s[i].toLowerCase());
+  }
+  return cleaned2.join("").split("-").join("");
+};
+
+const kebabCase = (s) => {
+  let cleaned3 = [];
+  for (let i = 0; i < s.length; i++) {
+    s[i] === s[i].toUpperCase()
+      ? cleaned3.push("-" + s[i].toLowerCase())
+      : cleaned3.push(s[i].toLowerCase());
+  }
+  return cleaned3.join("").split("_").join("");
+};
+
+const changeCase = (identifier, targetCase) => {
+  if (identifier.includes("-") && identifier.includes("_")) {
+    return undefined;
+  }
+  if (
+    (identifier.includes("-") || identifier.includes("_")) &&
+    identifier !== identifier.toLowerCase()
+  ) {
+    return undefined;
+  }
+  if (
+    targetCase !== "camel" &&
+    targetCase !== "snake" &&
+    targetCase !== "kebab"
+  )
+    return undefined;
+  if (targetCase === "camel") return camelCase(identifier);
+  if (targetCase === "snake") return snakeCase(identifier);
+  if (targetCase === "kebab") return kebabCase(identifier);
+};
+
+changeCase("some-lisp-name", "camel");
+//returns: "someLispName"
