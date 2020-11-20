f6db579788f21c708ada3173406bfa7f652ab345
SiEr0212 <simonertel@gmx.de>
2020-11-20 21:57:56 +0100

    Dashatize it

--- 6kyu/Dashatize_it.js
+++ 6kyu/Dashatize_it.js
@@ -3,5 +3,18 @@ Dashatize it
 
 from codewars: https://www.codewars.com/kata/58223370aef9fc03fd000071/train/javascript
 
+Dashatize it
+3158086% of 1,1202,782 of 7,674Dragoris3 Issues Reported
+JavaScript
+Node v8.1.3
+VIM
+EMACS
+Instructions
+Output
+Given a number, return a string with dash'-'marks before and after each odd integer, but do not begin or end the string with a dash mark.
 
+Ex:
+
+dashatize(274) -> '2-7-4'
+dashatize(6815) -> '68-1-5'
 */
