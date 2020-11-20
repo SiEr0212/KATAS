d0e208e9208782426f35eccef67dc8ba3fd37fa2
SiEr0212 <simonertel@gmx.de>
2020-11-20 21:58:58 +0100

    Dashatize it

--- 6kyu/Dashatize_it.js
+++ 6kyu/Dashatize_it.js
@@ -19,3 +19,30 @@ dashatize(274) -> '2-7-4'
 dashatize(6815) -> '68-1-5'
 */
 //My solution:
+
+const dashatize = (num) => {
+    if (isNaN(num)) return "NaN";
+    let number = num.toString();
+    if (number[0] === "-") number = number.split(number[0]).join("");
+    let res = "";
+    for (let i = 0; i < number.length; i++) {
+      parseInt(number[i]) % 2 !== 0 && number[0] !== "-"
+        ? (res += "-" + number[i] + "-")
+        : (res += number[i]);
+    }
+    let res2 = res.split("");
+    let res3 = [];
+    for (let j = 0; j < res2.length; j++) {
+      res2[j] == "-" && res2[j + 1] == "-" ? res2[j] : res3.push(res2[j]);
+    }
+    let res4 = res3.join("");
+    if (res4.charAt(0) === "-" && res4.charAt(res4.length - 1) !== "-")
+      return res4.substring(1);
+    if (res4.charAt(0) === "-" && res4.charAt(res4.length - 1) === "-")
+      return res4.substring(1, res4.length - 1);
+    if (res4.charAt(res4.length - 1) === "-")
+      return res4.substring(0, res4.length - 1);
+    return res4;
+  };
+
+  
