6badb7f22759de45c6df5d37852216a5e4043b86
SiEr0212 <simonertel@gmx.de>
2020-12-01 23:28:46 +0100

    Simple multiplication

--- 8kyu/Simple multiplication.js
+++ 8kyu/Simple multiplication.js
@@ -3,4 +3,5 @@ Simple multiplication
 
 from codewars: https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
 
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 */
