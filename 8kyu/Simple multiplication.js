08a13999784a8a7a2d1d1209dbfd1579a490800f
SiEr0212 <simonertel@gmx.de>
2020-12-01 23:29:59 +0100

    Simple multiplication

--- 8kyu/Simple multiplication.js
+++ 8kyu/Simple multiplication.js
@@ -10,3 +10,6 @@ This kata is about multiplying a given number by eight if it is an even number a
 const simpleMultiplication = (number) => {
     return number % 2 === 0 ? number * 8 : number * 9;
   };
+
+simpleMultiplication(1); 
+//,9,
