5a114017b4519888ff0e020a887c2d7102618fca
SiEr0212 <simonertel@gmx.de>
2020-12-01 23:29:03 +0100

    Simple multiplication

--- 8kyu/Simple multiplication.js
+++ 8kyu/Simple multiplication.js
@@ -5,3 +5,5 @@ from codewars: https://www.codewars.com/kata/583710ccaa6717322c000105/train/java
 
 This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 */
+//My solution:
+
