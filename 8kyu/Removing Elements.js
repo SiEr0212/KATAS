3ac1232ee058f26ff3ec931312bd12aaefe10fe3
SiEr0212 <simonertel@gmx.de>
2020-12-02 22:55:12 +0100

    Removing Elements

--- 8kyu/Removing Elements.js
+++ 8kyu/Removing Elements.js
@@ -3,5 +3,10 @@ Removing Elements
 
 from codewars: https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/javascript
 
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
 
+Example:
+
+myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
+None of the arrays will be empty, so you don't have to worry about that!
 */
