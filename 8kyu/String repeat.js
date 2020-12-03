be51c20713bf2932d606f2ff27f811ae3244dab8
SiEr0212 <simonertel@gmx.de>
2020-12-03 23:51:53 +0100

    String repeat

--- 8kyu/String repeat.js
+++ 8kyu/String repeat.js
@@ -3,7 +3,8 @@ String repeat
 
 from codewars: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
 
+Write a function called repeat_str which repeats the given string src exactly count times.
 
-
-
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
 */
