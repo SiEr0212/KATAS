cf34e43754ab82aa7f245c41604477118555d6ea
SiEr0212 <simonertel@gmx.de>
2020-12-09 23:06:17 +0100

    Fake Binary

--- 8kyu/Fake Binary.js
+++ 8kyu/Fake Binary.js
@@ -5,3 +5,13 @@ from codewars: https://www.codewars.com/kata/57eae65a4321032ce000002d/train/java
 
 Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 */
+//My solution:
+
+const fakeBin = (x) => {
+    let res = [];
+    for (let num of [...x]) {
+      console.log(num < 5);
+      num < 5 ? res.push("0") : res.push("1");
+    }
+    return res.join("");
+  };
