085b82ff7d923eb91b484468df5be34c65d30226
SiEr0212 <simonertel@gmx.de>
2020-12-17 23:29:00 +0100

    Reversed Strings

--- 8kyu/Reversed Strings.js
+++ 8kyu/Reversed Strings.js
@@ -3,5 +3,7 @@ Reversed Strings
 
 from codewars: https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
 
+Complete the solution so that it reverses the string passed into it.
 
+'world'  =>  'dlrow'
 */
