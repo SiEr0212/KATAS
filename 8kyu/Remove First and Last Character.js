187550195c2916a1111a1f2f05a5b5c79dcf5826
SiEr0212 <simonertel@gmx.de>
2020-12-19 23:50:00 +0100

    Remove First and Last Character

--- 8kyu/Remove First and Last Character.js
+++ 8kyu/Remove First and Last Character.js
@@ -7,4 +7,5 @@ It's pretty straightforward. Your goal is to create a function that removes the 
 last characters of a string. You're given one parameter, the original string. 
 You don't have to worry with strings with less than two characters.
 */
+//My solution:
 
