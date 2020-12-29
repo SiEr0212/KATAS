2d355f629acdc87933fb838c9e151ea47f16991e
SiEr0212 <simonertel@gmx.de>
2020-12-29 23:27:18 +0100

    Growth of a population

--- 7kyu/Growtth of a population.js
+++ 7kyu/Growtth of a population.js
@@ -32,3 +32,12 @@ Note: Don't forget to convert the percent parameter as a percentage in the body 
 if the parameter percent is 2 you have to convert it to 0.02.
 */
 //My solution:
+
+const nbYear = (p0, percent, aug, p) => {
+    let year = 0;
+    while (p0 < p) {
+      year++;
+      p0 = p0 + p0 * (percent / 100) + aug;
+    }
+    return year;
+  };
