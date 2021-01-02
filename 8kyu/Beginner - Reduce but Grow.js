f82cf80c1ac7ad4cfb388db5c814dbacde057106
SiEr0212 <simonertel@gmx.de>
2021-01-02 23:44:43 +0100

    Beginner - Reduce but Grow

--- 8kyu/Beginner - Reduce but Grow.js
+++ 8kyu/Beginner - Reduce but Grow.js
@@ -2,4 +2,8 @@
 Beginner - Reduce but Grow
 
 from codewars: https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 */
