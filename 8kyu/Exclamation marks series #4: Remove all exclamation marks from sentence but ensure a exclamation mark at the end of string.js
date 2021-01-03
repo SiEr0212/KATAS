3a3b84174fd909808b8b854549baf436dfb8fefe
SiEr0212 <simonertel@gmx.de>
2021-01-03 23:46:07 +0100

    Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string

--- 8kyu/Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string.js
+++ 8kyu/Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string.js
@@ -1,5 +1,6 @@
 /*
 Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
 
+from codewars: https://www.codewars.com/kata/57faf12b21c84b5ba30001b0/train/javascript
 
 */
