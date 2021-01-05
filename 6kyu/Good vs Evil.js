f175acd260a401441baad9b3a9f9af1e28764ed6
SiEr0212 <simonertel@gmx.de>
2021-01-05 23:56:41 +0100

    Good vs Evil

--- 6kyu/Good vs Evil.js
+++ 6kyu/Good vs Evil.js
@@ -49,3 +49,4 @@ Output:
 Return "Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, 
 or "Battle Result: No victor on this battle field" if it ends in a tie.
 */
+//My solution:
