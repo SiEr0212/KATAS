865050b76e5192b4f99376d1051c84d494b8f968
SiEr0212 <simonertel@gmx.de>
2021-01-06 00:23:32 +0100

    Good vs Evil

--- 6kyu/Good vs Evil.js
+++ 6kyu/Good vs Evil.js
@@ -70,3 +70,5 @@ const goodVsEvil = (good, evil) => {
       ? "Battle Result: Good triumphs over Evil"
       : "Battle Result: Evil eradicates all trace of Good";
   };
+
+  goodVsEvil("0 0 0 0 0 10", "0 1 1 1 1 0 0"); // === 'Battle Result: Good triumphs over Evil', 'Good should win' );
