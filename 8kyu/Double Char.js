b3804aa3968dc430aa74012f6bdc0e9c348fa0d0
SiEr0212 <simonertel@gmx.de>
2021-01-13 22:44:30 +0100

    Double Char

--- 8kyu/Double Char.js
+++ 8kyu/Double Char.js
@@ -1,4 +1,11 @@
 /*
 Double Char
 
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+doubleChar("1234!_ ") ==> "11223344!!__  "
 */
