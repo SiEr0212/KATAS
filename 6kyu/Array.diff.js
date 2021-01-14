3de582c8be1ea9c0377dca8013cef1839fe83080
SiEr0212 <simonertel@gmx.de>
2021-01-14 13:33:59 +0100

    Array.diff

--- 6kyu/Array.diff.js
+++ 6kyu/Array.diff.js
@@ -3,7 +3,8 @@ Array.diff
 
 from codewars: https://www.codewars.com/kata/523f5d21c841566fde000009/train/javascript
 
-our goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+our goal in this kata is to implement a difference function, 
+which subtracts one list from another and returns the result.
 
 It should remove all values from list a, which are present in list b.
 
