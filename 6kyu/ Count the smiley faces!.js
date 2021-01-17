71570b059feda05836c1dfb2bf872125aa6e1145
SiEr0212 <simonertel@gmx.de>
2021-01-17 21:22:24 +0100

    Count the smiley faces

--- 6kyu/ Count the smiley faces!.js
+++ 6kyu/ Count the smiley faces!.js
@@ -22,3 +22,4 @@ Note
 In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). 
 Order of the face (eyes, nose, mouth) elements will always be the same.
 */
+//My solution:
