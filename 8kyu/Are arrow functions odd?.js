91a768633b19cbd7e57fc9d6790b14c2cc9fcaaf
SiEr0212 <simonertel@gmx.de>
2021-01-24 23:46:24 +0100

    Are arrow functions odd?

--- 8kyu/Are arrow functions odd?.js
+++ 8kyu/Are arrow functions odd?.js
@@ -8,3 +8,7 @@ Time to test your basic knowledge in functions! Return the odds from a list:
 odds([1,2,3,4,5]) #=> [1,3,5]
 */
 //My solution:
+
+const odds = (values) => {
+    return values.filter((v) => v % 2);
+  };
