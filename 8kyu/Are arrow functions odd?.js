a7bf3999e1df22fc24874754f146bb25de6ce440
SiEr0212 <simonertel@gmx.de>
2021-01-24 23:44:51 +0100

    Are arrow functions odd?

--- 8kyu/Are arrow functions odd?.js
+++ 8kyu/Are arrow functions odd?.js
@@ -3,5 +3,7 @@ Are arrow functions odd?
 
 from codewara: https://www.codewars.com/kata/559f80b87fa8512e3e0000f5/train/javascript
 
+Time to test your basic knowledge in functions! Return the odds from a list:
 
+odds([1,2,3,4,5]) #=> [1,3,5]
 */
