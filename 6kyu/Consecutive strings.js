d9993f28cd13ed9502811f4c1bd77bee8bbf07b2
SiEr0212 <simonertel@gmx.de>
2021-01-31 23:48:37 +0100

    Consecutive strings

--- 6kyu/Consecutive strings.js
+++ 6kyu/Consecutive strings.js
@@ -29,3 +29,15 @@ Note
 consecutive strings : follow one after another without an interruption
 */
 //My solution:
+
+const longestConsec = (strarr, k) => {
+    let longest = "";
+    for (let i = 0; k > 0 && i <= strarr.length - k; i++) {
+      let tempStr = strarr.slice(i, i + k).join("");
+      console.log(tempStr.length);
+      if (tempStr.length > longest.length) {
+        longest = tempStr;
+      }
+    }
+    return longest;
+  };
