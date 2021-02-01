4866e80dcf104baa2f2535ea210cf3e435200f22
SiEr0212 <simonertel@gmx.de>
2021-02-01 23:38:11 +0100

    Do I get a bonus?

--- 8kyu/Do I get a bonus?.js
+++ 8kyu/Do I get a bonus?.js
@@ -12,3 +12,4 @@ If bonus is true, the salary should be multiplied by 10. If bonus is false, the 
 Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java and Julia), 
 "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell and Lua) or "¥" (Rust).
 */
+//My solution:
