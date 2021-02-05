d17d75c65bf86650b867d8b70ee6c10bf6c48f80
SiEr0212 <simonertel@gmx.de>
2021-02-05 23:57:50 +0100

    Magic Highschool

--- 6kyu/Magic Highschool: Your first potion.js
+++ 6kyu/Magic Highschool: Your first potion.js
@@ -73,4 +73,4 @@ const getIngredients = (recipes, final) => {
 };
 
 getIngredients(recipes, "invisibility potion"); 
-//{"dewdrop": 12, "troll nail": 6, "siren teardrop": 1},
+//returns: {"dewdrop": 12, "troll nail": 6, "siren teardrop": 1},
