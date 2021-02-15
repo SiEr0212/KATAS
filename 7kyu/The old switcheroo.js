7592bb0b6ce5d64b5ab2da4cce9073f222e6331c
SiEr0212 <simonertel@gmx.de>
2021-02-15 23:49:45 +0100

    The old switcheroo

--- 7kyu/The old switcheroo.js
+++ 7kyu/The old switcheroo.js
@@ -30,4 +30,5 @@ const vowel2index = (str) => {
     return res;
   };
 
-  vowel2index("this is my string"); //,'th3s 6s my str15ng')
+  vowel2index("this is my string"); 
+  //,'th3s 6s my str15ng')
