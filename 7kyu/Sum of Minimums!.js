e93acc74f92a5127aba79992f67872b3783db184
SiEr0212 <simonertel@gmx.de>
2021-02-19 23:54:24 +0100

    Sum of Minimums

--- 7kyu/Sum of Minimums!.js
+++ 7kyu/Sum of Minimums!.js
@@ -23,4 +23,9 @@ const sumOfMinimums = (arr) => {
   return res.reduce((prev, curr) => prev + curr);
 };
 
-
+sumOfMinimums([
+    [7, 9, 8, 6, 2],
+    [6, 3, 5, 4, 3],
+    [5, 8, 7, 4, 5],
+  ]); //, 9));
+  
