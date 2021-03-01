6a3c62ba485066a551935ab8cb35d0951ce32774
SiEr0212 <simonertel@gmx.de>
2021-03-01 21:32:47 +0100

    Guess the Sequence

--- 7kyu/Guess the Sequence.js
+++ 7kyu/Guess the Sequence.js
@@ -12,3 +12,10 @@ result = [1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9]
 Good luck!
  */
 //My solution:
+
+const sequence = (x) => {
+    let res = [];
+    for (let i = 1; i <= x; i++) res.push(i);
+    return res.sort();
+  };
+
