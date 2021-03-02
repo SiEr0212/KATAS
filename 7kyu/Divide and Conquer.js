c143273c5596e260494a0ad1445ec29518b641c7
SiEr0212 <simonertel@gmx.de>
2021-03-02 21:36:25 +0100

    Divide and Conquer

--- 7kyu/Divide and Conquer.js
+++ 7kyu/Divide and Conquer.js
@@ -7,3 +7,15 @@ Given a mixed array of number and string representations of integers,
 add up the string integers and subtract this from the total of the non-string integers.
  */
 //My solution:
+
+const divCon = (x) => {
+    let num = [];
+    let string = [];
+     x.forEach((el) => (typeof el === "number" ? num.push(el) : string.push(el)));
+    if (num.length === 0) num = [0];
+     if (string.length === 0) string = [0];
+    return (
+      num.reduce((a, c) => a + c) -
+      string.map((s) => Number(s)).reduce((a, c) => a + c)
+    );
+  }
