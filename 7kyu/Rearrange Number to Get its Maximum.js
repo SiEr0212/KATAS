3cb83608965da04c494522b724235205f77ed7d6
SiEr0212 <simonertel@gmx.de>
2021-03-03 20:19:36 +0100

    Rearrange Number to Get its Maximum

--- 7kyu/Rearrange Number to Get its Maximum.js
+++ 7kyu/Rearrange Number to Get its Maximum.js
@@ -3,6 +3,9 @@ Rearrange Number to Get its Maximum
 
 from codewars: https://www.codewars.com/kata/563700da1ac8be8f1e0000dc/train/javascript
 
+Create function that takes one positive three digit integer and rearranges its digits to get maximum possible number.
+Assume that argument is integer. Return null (nil for ruby) if argument is not valid.
 
+maxRedigit(123); // returns 321
 */
   
