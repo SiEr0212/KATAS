88cd7b7646bf69881335dea6d998899af776aa4c
SiEr0212 <simonertel@gmx.de>
2021-03-06 23:33:54 +0100

    Reverse words

--- 7kyu/Reverse words.js
+++ 7kyu/Reverse words.js
@@ -2,4 +2,12 @@
 Reverse words
 
 form codewars: https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/javascript
+
+Complete the function that accepts a string parameter, 
+and reverses each word in the string.
+All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
 */
