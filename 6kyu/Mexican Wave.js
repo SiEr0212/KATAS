aa05999cf64e976c14e6315accca848cd17e677e
SiEr0212 <simonertel@gmx.de>
2021-03-19 21:50:02 +0100

    Mexican Wave

--- 6kyu/Mexican Wave.js
+++ 6kyu/Mexican Wave.js
@@ -30,17 +30,16 @@ Good luck and enjoy!
 //My solution:
 
 const wave = (str) => {
-    return Array(str.length)
-      .fill(str)
-      .map(
-        (v, i, arr) =>
-          v.slice(0, i).toLowerCase() +
-          v.slice(i, i + 1).toUpperCase() +
-          v.slice(i + 1).toLowerCase()
-      )
-      .filter((v) => v != v.toLowerCase());
-  };
-  
+  return Array(str.length)
+    .fill(str)
+    .map(
+      (v, i, arr) =>
+        v.slice(0, i).toLowerCase() +
+        v.slice(i, i + 1).toUpperCase() +
+        v.slice(i + 1).toLowerCase()
+    )
+    .filter((v) => v != v.toLowerCase());
+};
+
 wave("two words");
 //returns: ['Two words', 'tWo words', 'twO words', 'two Words', 'two wOrds', 'two woRds', 'two worDs', 'two wordS']
-
