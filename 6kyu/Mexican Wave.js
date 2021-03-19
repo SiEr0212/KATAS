f71dc6b15b5501bdc10be996371aa08fe29d483d
SiEr0212 <simonertel@gmx.de>
2021-03-19 21:47:56 +0100

    Mexican Wave

--- 6kyu/Mexican Wave.js
+++ 6kyu/Mexican Wave.js
@@ -28,3 +28,16 @@ wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
 Good luck and enjoy!
  */
 //My solution:
+
+const wave = (str) => {
+    return Array(str.length)
+      .fill(str)
+      .map(
+        (v, i, arr) =>
+          v.slice(0, i).toLowerCase() +
+          v.slice(i, i + 1).toUpperCase() +
+          v.slice(i + 1).toLowerCase()
+      )
+      .filter((v) => v != v.toLowerCase());
+  };
+  
