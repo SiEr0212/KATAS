c031706f965b16754b342ed5b3f79d7b3b64ac61
SiEr0212 <simonertel@gmx.de>
2021-03-21 23:57:24 +0100

    Ce*s*r*d Strings

--- 7kyu/ Ce*s*r*d Strings.js
+++ 7kyu/ Ce*s*r*d Strings.js
@@ -7,7 +7,8 @@ My PC got infected by a strange virus. It only infects my text files and replace
 
 Fortunately, I discovered that the virus hides my censored letters inside root directory.
 
-It will be very tedious to recover all these files manually, so your goal is to implement uncensor function that does the hard work automatically.
+It will be very tedious to recover all these files manually, 
+so your goal is to implement uncensor function that does the hard work automatically.
 
 Examples
 uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae") ➜ "This is very strange"
