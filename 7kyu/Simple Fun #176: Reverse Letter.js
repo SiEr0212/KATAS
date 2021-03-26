be0d2f6c8b859b8c6b50d5e2f1a809cc9d4f7f55
SiEr0212 <simonertel@gmx.de>
2021-03-26 23:57:04 +0100

    Simple Fun #176: Reverse Letter

--- 7kyu/Simple Fun #176: Reverse Letter.js
+++ 7kyu/Simple Fun #176: Reverse Letter.js
@@ -3,15 +3,15 @@ Simple Fun #176: Reverse Letter
 
 from codewars: https://www.codewars.com/kata/58b8c94b7df3f116eb00005b/train/javascript
 
-ask
+Task:
 Given a string str, reverse it omitting all non-alphabetic characters.
 
-Example
+Example:
 For str = "krishan", the output should be "nahsirk".
 
 For str = "ultr53o?n", the output should be "nortlu".
 
-Input/Output
+Input/Output:
 [input] string str
 
 A string consists of lowercase latin letters, digits and symbols.
