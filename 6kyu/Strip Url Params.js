6c241dc83be839de8c419dda632edec51cf17810
SiEr0212 <simonertel@gmx.de>
2021-03-30 23:59:29 +0200

    Strip Url Params

--- 6kyu/Strip Url Params.js
+++ 6kyu/Strip Url Params.js
@@ -2,4 +2,12 @@
 Strip Url Params
 
 from codewars: https://www.codewars.com/kata/51646de80fd67f442c000013/train/javascript
+
+Complete the method so that it does the following:
+Removes any duplicate query string parameters from the url (the first occurence should be kept)
+Removes any query string parameters specified within the 2nd argument (optional array)
+Examples:
+stripUrlParams('www.codewars.com?a=1&b=2&a=2') === 'www.codewars.com?a=1&b=2'
+stripUrlParams('www.codewars.com?a=1&b=2&a=2', ['b']) === 'www.codewars.com?a=1'
+stripUrlParams('www.codewars.com', ['b']) === 'www.codewars.com'
  */
