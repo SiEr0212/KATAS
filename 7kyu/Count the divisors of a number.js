044c83958416b90e08707fcff001309451b2df66
SiEr0212 <simonertel@gmx.de>
2021-08-15 19:49:16 +0200

    Count the divisors of a number

--- 7kyu/Count the divisors of a number.js
+++ 7kyu/Count the divisors of a number.js
@@ -12,3 +12,11 @@ divisors(12) = 6  // 1, 2, 3, 4, 6, 12
 divisors(30) = 8  // 1, 2, 3, 5, 6, 10, 15, 30
  */
 //my solution:
+
+const getDivisorsCnt = (n) => {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      n % i === 0 ? sum += 1 : null
+    }
+    return sum;
+  };
