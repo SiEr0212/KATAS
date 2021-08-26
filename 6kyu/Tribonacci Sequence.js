ee51bc2801683478cabd48d4314b4c687769ae9b
SiEr0212 <simonertel@gmx.de>
2021-08-26 20:21:49 +0200

    Tribonacci Sequence

--- 6kyu/Tribonacci Sequence.js
+++ 6kyu/Tribonacci Sequence.js
@@ -21,3 +21,4 @@ returns the first n elements - signature included of the so seeded sequence.
 Signature will always contain 3 numbers; n will always be a non-negative number; 
 if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
 */
+//My solution:
