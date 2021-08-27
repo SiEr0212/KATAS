93d8a76e0daf8a16fe99cc916b761e69dc064572
SiEr0212 <simonertel@gmx.de>
2021-08-27 22:11:16 +0200

    Is it a Palindrome?

--- 8kyu/Is it a Palindrome?.js
+++ 8kyu/Is it a Palindrome?.js
@@ -1,3 +1,6 @@
 /* 
 Is it a Palindrome?
+
+Write function that checks if a given string (case insensitive) is a palindrome (a word, phrase, or sequence that reads the same backwards as forwards, e.g. madam or nurses run.)
+
  */
