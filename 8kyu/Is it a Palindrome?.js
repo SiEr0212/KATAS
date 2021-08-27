94875eb5d19234cbac2b890aca54f66c110c1195
SiEr0212 <simonertel@gmx.de>
2021-08-27 22:13:55 +0200

    Is it a Palindrome?

--- 8kyu/Is it a Palindrome?.js
+++ 8kyu/Is it a Palindrome?.js
@@ -4,7 +4,7 @@ Is it a Palindrome?
 Write function that checks if a given string (case insensitive) is a palindrome 
 (a word, phrase, or sequence that reads the same backwards as forwards, e.g. madam or nurses run.).
 */
-//my solution
+//my solution:
 
 const isPalindrome = (x) =>
   x.toLowerCase() === x.toLowerCase().split("").reverse().join("");
