18cb9bbb97d6d43e8e679e95c9d05bf20ace4610
SiEr0212 <simonertel@gmx.de>
2021-08-31 21:59:47 +0200

    Is n divisible by (...)?

--- 7kyu/Is n divisible by (...)?.js
+++ 7kyu/Is n divisible by (...)?.js
@@ -1,7 +1,8 @@
 /* 
 Is n divisible by (...)?
 
-Create a function isDivisible(n,...) that checks if the first argument n is divisible by all other arguments (return true if no other arguments)
+Create a function isDivisible(n,...) that checks if the first argument n is divisible by all other arguments 
+(return true if no other arguments)
 
 Example:
 
