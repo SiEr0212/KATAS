d68ab79cfaba696493becef1c5d50aa1ce23d8b1
SiEr0212 <simonertel@gmx.de>
2021-09-04 23:48:25 +0200

    Twice as old

--- 8kyu/Twice as old.js
+++ 8kyu/Twice as old.js
@@ -3,8 +3,9 @@ Twice as old
 
 Your function takes two arguments:
 
-current father's age (years)
-current age of his son (years)
-Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+1.current father's age (years)
+2.current age of his son (years)
 
+Сalculate how many years ago the father was twice as old as his son 
+(or in how many years he will be twice as old).
 */
