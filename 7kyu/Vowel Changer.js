e6384fdac5e8da2d363800d855b8bd7c35c6c891
SiEr0212 <simonertel@gmx.de>
2021-09-05 23:39:54 +0200

    Vowel Changer

--- 7kyu/Vowel Changer.js
+++ 7kyu/Vowel Changer.js
@@ -11,7 +11,8 @@ For Example (input1, vowel) => output:
 ("hannah hannah bo-bannah banana fanna fo-fannah fee, fy, mo-mannah. hannah!",'i') => 
 'hinnih hinnih bi-binnih binini finni fi-finnih fii, fy, mi-minnih. hinnih!'
 
-('adira wants to go to the park', 'o') =>'odoro wonts to go to tho pork'
+('adira wants to go to the park', 'o')
+ =>'odoro wonts to go to tho pork'
 There will never be an uppercase letter as an input.
 */
 //My solution:
