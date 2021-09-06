e19a45d8e3a90c83c1dd1d70f2c6eb98697b1b33
SiEr0212 <simonertel@gmx.de>
2021-09-06 23:23:38 +0200

    Largest pair sunm in array

--- 7kyu/Largest pair sum in array.js
+++ 7kyu/Largest pair sum in array.js
@@ -3,4 +3,10 @@ Largest pair sum in array
 
 from codewars: https://www.codewars.com/kata/556196a6091a7e7f58000018/train/javascript
 
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
 */
