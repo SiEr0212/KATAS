c7f90252ef34f824b00723c1aa69e8ce2ce2dd2a
SiEr0212 <simonertel@gmx.de>
2021-09-07 23:46:24 +0200

    Jaden Casing Strings

--- 7kyu/Jaden Casing Strings.js
+++ 7kyu/Jaden Casing Strings.js
@@ -8,7 +8,8 @@ Jaden is also known for some of his philosophy that he delivers via Twitter. Whe
 he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, 
 check out how contractions are expected to be in the example below.
 
-Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+Your task is to convert strings to how they would be written by Jaden Smith. 
+The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
 
 Example:
 
