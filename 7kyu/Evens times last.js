217fdbba0816ed92df664d6052518153f5b917e5
SiEr0212 <simonertel@gmx.de>
2021-09-11 23:56:35 +0200

    Evens times last

--- 7kyu/Evens times last.js
+++ 7kyu/Evens times last.js
@@ -10,14 +10,16 @@ Indices in sequence start from 0.
 
 If the sequence is empty, you should return 0.
 */
- //My solution:
+//My solution:
 
- const evenLast = (numbers) => {
-    let evens = [];
-    for (let i = 0; i < numbers.length; i++) {
-      i % 2 === 0 ? evens.push(numbers[i]) : null;
-    }
-    return evens.length === 0 ? 0 : evens.reduce((prev, curr) => prev + curr) * numbers.slice(-1)[0];
-  };
+const evenLast = (numbers) => {
+  let evens = [];
+  for (let i = 0; i < numbers.length; i++) {
+    i % 2 === 0 ? evens.push(numbers[i]) : null;
+  }
+  return evens.length === 0
+    ? 0
+    : evens.reduce((prev, curr) => prev + curr) * numbers.slice(-1)[0];
+};
 
-  evenLast([2, 3, 4, 5])// returns: 30
+evenLast([2, 3, 4, 5]); // returns: 30
