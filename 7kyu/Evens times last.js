0de7bdee7b4a52c71980e081d591090a86a6bff3
SiEr0212 <simonertel@gmx.de>
2021-09-11 23:54:56 +0200

    Evens times last

--- 7kyu/Evens times last.js
+++ 7kyu/Evens times last.js
@@ -3,4 +3,9 @@ Evens times last
 
 from codewars: https://www.codewars.com/kata/5a1a9e5032b8b98477000004/train/javascript
 
+Given a sequence of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
+
+Indices in sequence start from 0.
+
+If the sequence is empty, you should return 0.
 */
