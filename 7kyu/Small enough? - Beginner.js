311e1a6b7386404409c90c2184d45f82dc076448
SiEr0212 <simonertel@gmx.de>
2021-09-12 23:39:15 +0200

    Small enough? - Beginner

--- 7kyu/Small enough? - Beginner.js
+++ 7kyu/Small enough? - Beginner.js
@@ -12,11 +12,11 @@ You can assume all values in the array are numbers.
 //My solution:
 
 const smallEnough = (a, limit) => {
-    let check = 0;
-    for (let el of a) {
-      el <= limit ? ++check : --check;
-    }
-    return check === a.length;
-  };
-  
-  smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120); // returns: true
+  let check = 0;
+  for (let el of a) {
+    el <= limit ? ++check : --check;
+  }
+  return check === a.length;
+};
+
+smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120); // returns: true
