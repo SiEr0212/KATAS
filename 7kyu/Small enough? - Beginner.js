8c59b7201c8c2376cf8782801a80a9abe28cb7d3
SiEr0212 <simonertel@gmx.de>
2021-09-12 23:41:15 +0200

    Small enough? - Beginner

--- 7kyu/Small enough? - Beginner.js
+++ 7kyu/Small enough? - Beginner.js
@@ -20,5 +20,4 @@ const smallEnough = (a, limit) => {
   return check === a.length;
 };
 
-smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120); 
-//returns: true
+smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120); //returns: true
