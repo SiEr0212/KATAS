b3be5ac9c984fc27977ab62adca6456e399a60e5
SiEr0212 <simonertel@gmx.de>
2021-09-14 22:09:18 +0200

    Highest Scoring Word

--- 6kyu/ Highest Scoring Word.js
+++ 6kyu/ Highest Scoring Word.js
@@ -1,5 +1,7 @@
 /* 
 Highest Scoring Word
 
-from codewars: https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/solutions/javascript
+from codewars: https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/javascript
+
+
 */
