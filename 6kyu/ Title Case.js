26db309f3b84d7ce2f77ce14271685fa8ff365cc
SiEr0212 <simonertel@gmx.de>
2021-09-16 23:58:32 +0200

    Title Case

--- 6kyu/ Title Case.js
+++ 6kyu/ Title Case.js
@@ -27,6 +27,7 @@ titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings
 titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
 titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
  */
+//My solution:
 
 const titleCase = (title, minorWords) => {
   let titleCased = title
