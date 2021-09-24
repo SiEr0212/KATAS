23670752f0769b7df99b20a14075f59c47f142d5
SiEr0212 <simonertel@gmx.de>
2021-09-24 22:30:17 +0200

    VAPORCODE

--- 7kyu/VAPORCODE.js
+++ 7kyu/VAPORCODE.js
@@ -15,6 +15,7 @@ Examples
 "Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
 "Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
 */
+
 //My solution:
 
 const vaporcode = (string) => {
