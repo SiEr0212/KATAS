9c92fe836c8381b09ba1f7f029cee365d6def7a7
SiEr0212 <simonertel@gmx.de>
2021-09-24 22:25:11 +0200

    VAPORCODE

--- 7kyu/VAPORCODE.js
+++ 7kyu/VAPORCODE.js
@@ -15,3 +15,4 @@ Examples
 "Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
 "Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
 */
+//My solution:
