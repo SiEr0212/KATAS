62e9ea1cf5a969ae391d1dd4cb07f7eeffbf33b1
SiEr0212 <simonertel@gmx.de>
2021-09-24 22:25:00 +0200

    VAPORCODE

--- 7kyu/VAPORCODE.js
+++ 7kyu/VAPORCODE.js
@@ -6,7 +6,8 @@ from codewars: https://www.codewars.com/kata/5966eeb31b229e44eb00007a/train/java
 ASC Week 1 Challenge 4 (Medium #1)
 
 Write a function that converts any sentence into a V A P O R W A V E sentence. 
-A V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+A V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) 
+to create this V A P O R W A V E effect.
 
 Note that spaces should be ignored in this case.
 
