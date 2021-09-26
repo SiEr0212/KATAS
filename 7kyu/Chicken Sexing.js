5a0ce9af7b84d35fb7772e1ce40d71026378994a
SiEr0212 <simonertel@gmx.de>
2021-09-26 23:58:58 +0200

    Chicken Sexing

--- 7kyu/Chicken Sexing.js
+++ 7kyu/Chicken Sexing.js
@@ -3,7 +3,8 @@ Chicken Sexing
 
 fromn codewars: https://www.codewars.com/kata/57ed40e3bd793e9c92000fcb/train/javascript
 
-Bob is a chicken sexer. His job is to sort baby chicks into a Male(M) and Female(F) piles. When bob can't guess can throw his hands up and declare it with a '?'.
+Bob is a chicken sexer. His job is to sort baby chicks into a Male(M) and Female(F) piles. 
+When bob can't guess can throw his hands up and declare it with a '?'.
 
 Bob's bosses don't trust Bob's ability just yet, so they have paired him with an expert sexer. All of Bob's decisions will be checked against the experts choices to generate a correctness score.
 
