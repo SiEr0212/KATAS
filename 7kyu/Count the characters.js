907b3cefe175f3e69b4c03af23a3e0aff6601730
SiEr0212 <simonertel@gmx.de>
2021-09-28 22:40:02 +0200

    Count the characters

--- 7kyu/Count the characters.js
+++ 7kyu/Count the characters.js
@@ -4,7 +4,8 @@ Count hte characters
 from codewars: https://www.codewars.com/kata/577ad961ae2807182f000c29/train/javascript
 
 The goal of this kata is to write a function that takes two inputs: 
-a string and a character. The function will count the number of times that character appears in the string. The count is case insensitive.
+a string and a character. The function will count the number of times that character appears in the string. 
+The count is case insensitive.
 
 For example:
 
