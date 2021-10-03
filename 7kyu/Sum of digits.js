77e86bb813b3a702ea73c8094bd2f43281ac0df4
SiEr0212 <simonertel@gmx.de>
2021-10-03 23:39:54 +0200

    Sum of digits

--- 7kyu/Sum of digits.js
+++ 7kyu/Sum of digits.js
@@ -3,4 +3,10 @@ Sum of digits
 
 from codewars:https://www.codewars.com/kata/59cf805aaeb28438fe00001c/train/javascript
 
+It involves implementing a program that sums the digits of a non-negative integer. For example, the sum of 3433 digits is 13.
+
+Digits can be a number, a string, or undefined. In case of undefined return an empty string ''.
+
+To give you a little more excitement, the program will not only write the result of the sum, 
+but also write all the sums used: 3 + 4 + 3 + 3 = 13.
 */
