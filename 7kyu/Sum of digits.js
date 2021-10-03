dceab52bce626fd38c9970461be56a5353343d13
SiEr0212 <simonertel@gmx.de>
2021-10-03 23:42:58 +0200

    Sum of digits

--- 7kyu/Sum of digits.js
+++ 7kyu/Sum of digits.js
@@ -8,7 +8,8 @@ For example, the sum of 3433 digits is 13.
 
 Digits can be a number, a string, or undefined. In case of undefined return an empty string ''.
 
-To give you a little more excitement, the program will not only write the result of the sum, 
+To give you a little more excitement, 
+the program will not only write the result of the sum, 
 but also write all the sums used: 3 + 4 + 3 + 3 = 13.
 */
 //My solution:
