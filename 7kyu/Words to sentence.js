e53067d9add2dc68931e6ab6dabf2c33bbc0c342
SiEr0212 <simonertel@gmx.de>
2021-10-04 23:10:28 +0200

    Words to sentence

--- 7kyu/Words to sentence.js
+++ 7kyu/Words to sentence.js
@@ -2,4 +2,10 @@
 Words to sentence
 
 from codewars: https://www.codewars.com/kata/57a06005cf1fa5fbd5000216/train/javascript
+
+Write function which will create a string from a list of strings, separated by space.
+
+Example:
+
+["hello", "world"] -> "hello world"
  */
