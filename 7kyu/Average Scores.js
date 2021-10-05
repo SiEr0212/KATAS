0c00e1d8e4696b19b39bd4e39dae8be8432b3f9d
SiEr0212 <simonertel@gmx.de>
2021-10-05 23:50:17 +0200

    Average Scores

--- 7kyu/Average Scores.js
+++ 7kyu/Average Scores.js
@@ -3,5 +3,6 @@ Average Score's
 
 from  codewars: https://www.codewars.com/kata/57b68bc7b69bfc8209000307/train/javascript
 
-Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. 
+You are not allowed to use any loops (including for, for/in, while, and do/while loops).
 */
