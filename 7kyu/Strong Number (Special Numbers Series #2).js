e3843ecd2c0766d556160791653917f15ff20d5f
SiEr0212 <simonertel@gmx.de>
2021-10-13 23:54:58 +0200

    Strong Number (Special Numbers Series #2)

--- 7kyu/Strong Number (Special Numbers Series #2).js
+++ 7kyu/Strong Number (Special Numbers Series #2).js
@@ -2,4 +2,14 @@
 Strong Number (Special Numbers Series #2) 
 
 from codewars:https://www.codewars.com/kata/5a4d303f880385399b000001/train/javascript
+
+Definition
+Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+
+For example: 145, since
+1! + 4! + 5! = 1 + 24 + 120 = 145
+So, 145 is a Strong number.
+
+Task
+Given a number, Find if it is Strong or not.
 */
