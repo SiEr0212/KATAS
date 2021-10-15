5f091663f21d54db7a84c3afe009ad3e06c74941
SiEr0212 <simonertel@gmx.de>
2021-10-15 21:45:49 +0200

    Odd Ones Out

--- 7kyu/ Odd Ones Out!.js
+++ 7kyu/ Odd Ones Out!.js
@@ -9,7 +9,8 @@ However you can distinguish the family they belong to by the number on the shirt
 As the sheriff's assistant it's your job to find all the odd numbered families and remove them from the town!
 
 Challenge: You are given a list of numbers. 
-The numbers each repeat a certain number of times. Remove all numbers that repeat an odd number of times while keeping everything else the same.
+The numbers each repeat a certain number of times. 
+Remove all numbers that repeat an odd number of times while keeping everything else the same.
 
 oddOnesOut([1, 2, 3, 1, 3, 3]) = [1, 1]
 In the above example:
