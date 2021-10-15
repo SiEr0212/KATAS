599e272d24fd9a63a282c4446c7d31a27c969b6a
SiEr0212 <simonertel@gmx.de>
2021-10-15 21:45:40 +0200

    Odd Ones Out

--- 7kyu/ Odd Ones Out!.js
+++ 7kyu/ Odd Ones Out!.js
@@ -5,7 +5,8 @@ from Codewars: https://www.codewars.com/kata/5d376cdc9bcee7001fcb84c0/train/java
 
 The town sheriff dislikes odd numbers and wants all odd numbered families out of town! 
 In town crowds can form and individuals are often mixed with other people and families. 
-However you can distinguish the family they belong to by the number on the shirts they wear. As the sheriff's assistant it's your job to find all the odd numbered families and remove them from the town!
+However you can distinguish the family they belong to by the number on the shirts they wear. 
+As the sheriff's assistant it's your job to find all the odd numbered families and remove them from the town!
 
 Challenge: You are given a list of numbers. 
 The numbers each repeat a certain number of times. Remove all numbers that repeat an odd number of times while keeping everything else the same.
