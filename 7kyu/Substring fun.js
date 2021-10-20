113b04f8a7ad9b7d732312f489f6a0b46540325b
SiEr0212 <simonertel@gmx.de>
2021-10-21 00:20:42 +0200

    Substring fun

--- 7kyu/Substring fun.js
+++ 7kyu/Substring fun.js
@@ -19,9 +19,11 @@ i.e. a string array or an empty array; and each word will have enough letters.
 //My solution:
 
 const nthChar = (words) => {
-  for (let word of words) {
-  }
-  return words;
-};
+    let str='';
+      for (let i=0;i<words.length;i++){
+      str+=words[i][i]
+      }
+     return words.length ? str : ''
+    }
 
 nthChar(["yoda", "best", "has"]); //returns: 'yes'
