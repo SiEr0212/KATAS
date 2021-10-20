634cc4ffd48bb0046a534a066d17e0ffd8dc6d56
SiEr0212 <simonertel@gmx.de>
2021-10-20 23:55:15 +0200

    Substring fun

--- 7kyu/Substring fun.js
+++ 7kyu/Substring fun.js
@@ -2,4 +2,15 @@
 Substring fun
 
 from codewars: https://www.codewars.com/kata/565b112d09c1adfdd500019c/train/javascript
+
+Complete the function that takes an array of words.
+
+You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
+
+For example:
+
+["yoda", "best", "has"]  -->  "yes"
+  ^        ^        ^
+  n=0     n=1     n=2
+Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters.
 */
