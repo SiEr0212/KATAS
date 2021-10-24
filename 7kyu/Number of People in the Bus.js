5003435c44e0fca8d984e9672530fd111cc917d6
SiEr0212 <simonertel@gmx.de>
2021-10-24 23:10:23 +0200

    Number of People in the Bus

--- 7kyu/Number of People in the Bus.js
+++ 7kyu/Number of People in the Bus.js
@@ -1,5 +1,5 @@
 /*  
 Number of People in the Bus
 
-froom codewars: https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/javascript
+from codewars: https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/javascript
 */
