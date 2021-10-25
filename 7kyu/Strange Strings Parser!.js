9d59c1f38400a343a059f64837863a41a02e8425
SiEr0212 <simonertel@gmx.de>
2021-10-25 23:53:41 +0200

    Strange Strings Parser

--- 7kyu/Strange Strings Parser!.js
+++ 7kyu/Strange Strings Parser!.js
@@ -8,3 +8,4 @@ It seems to be separated by a random special character! Oh No!
 We need your help to find the special character, parse the data, and return it translated! 
 There isn't much time, hurry we need your help!
  */
+//My solution:
