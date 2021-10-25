6f5ff4a5b725d2f91469f32ccc66c5d67046f762
SiEr0212 <simonertel@gmx.de>
2021-10-25 23:53:31 +0200

    Strange Strings Parser

--- 7kyu/Strange Strings Parser!.js
+++ 7kyu/Strange Strings Parser!.js
@@ -5,5 +5,6 @@ from codewars: https://www.codewars.com/kata/584d88622609c8bda30000cf/train/java
 
 Here you have a connected to a socket, and the data looks very strange. 
 It seems to be separated by a random special character! Oh No! 
-We need your help to find the special character, parse the data, and return it translated! There isn't much time, hurry we need your help!
+We need your help to find the special character, parse the data, and return it translated! 
+There isn't much time, hurry we need your help!
  */
