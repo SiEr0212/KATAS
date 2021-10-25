39cf6898690ecaffbad715006ef380b80b5a1e84
SiEr0212 <simonertel@gmx.de>
2021-10-25 23:53:14 +0200

    Strange Strings Parser

--- 7kyu/Strange Strings Parser!.js
+++ 7kyu/Strange Strings Parser!.js
@@ -3,5 +3,6 @@ Strange Strings Parser!
 
 from codewars: https://www.codewars.com/kata/584d88622609c8bda30000cf/train/javascript
 
-Here you have a connected to a socket, and the data looks very strange. It seems to be separated by a random special character! Oh No! We need your help to find the special character, parse the data, and return it translated! There isn't much time, hurry we need your help!
+Here you have a connected to a socket, and the data looks very strange. 
+It seems to be separated by a random special character! Oh No! We need your help to find the special character, parse the data, and return it translated! There isn't much time, hurry we need your help!
  */
