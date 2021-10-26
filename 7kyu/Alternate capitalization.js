b71547517b697b615091762f8b6b53e46d8ef52c
SiEr0212 <simonertel@gmx.de>
2021-10-26 23:56:10 +0200

    Alternate capitalization

--- 7kyu/Alternate capitalization.js
+++ 7kyu/Alternate capitalization.js
@@ -7,7 +7,8 @@ Given a string, capitalize the letters that occupy even indexes and odd indexes 
 and return as shown below. 
 Index 0 will be considered even.
 
-For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. 
+'See test cases for more examples.
 
 The input will be a lowercase string with no spaces.
 
