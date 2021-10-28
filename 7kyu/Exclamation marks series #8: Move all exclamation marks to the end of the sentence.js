77c11cb45084e66b6f915f407b91f57fdafcd051
SiEr0212 <simonertel@gmx.de>
2021-10-28 23:20:10 +0200

    Exclamation marks series #8: Move all exclamation marks to the end of the sentence

--- 7kyu/Exclamation marks series #8: Move all exclamation marks to the end of the sentence.js
+++ 7kyu/Exclamation marks series #8: Move all exclamation marks to the end of the sentence.js
@@ -1,5 +1,5 @@
 /*  
 Exclamation marks series #8: Move all exclamation marks to the end of the sentence
 
-from codewars: 
+from codewars: https://www.codewars.com/kata/57fafd0ed80daac48800019f/train/javascript
 */
