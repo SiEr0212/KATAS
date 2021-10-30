b86df9350c0a2de580802a95038d039d78157ee5
SiEr0212 <simonertel@gmx.de>
2021-10-30 23:25:52 +0200

    Thinking & Testing : How many word?

--- 7kyu/Thinking & Testing : How many "word"?.js
+++ 7kyu/Thinking & Testing : How many "word"?.js
@@ -2,4 +2,12 @@
 Thinking & Testing : How many "word"?
 
 from codwars: https://www.codewars.com/kata/56eff1e64794404a720002d2/train/javascript
+
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
 */
