775a52172e877e39c6373ee993b05d7a5b52b90d
SiEr0212 <simonertel@gmx.de>
2021-11-01 23:57:40 +0100

    Find the smallest integer in the array

--- 8kyu/Find the smallest integer in the array.js
+++ 8kyu/Find the smallest integer in the array.js
@@ -16,3 +16,5 @@ You can assume, for the purpose of this kata, that the supplied array will not b
 const findSmallestInt = (args) => {
     return args.sort((a, b) => a - b)[0];
   }
+
+findSmallestInt([78,56,232,12,8])//,8
